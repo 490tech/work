36633c48a943fa276d91950b81480cd69334b017
490tech <66438386+490tech@users.noreply.github.com>
2021-11-15 20:57:20 +0300

    Update connector.py

--- connector.py
+++ connector.py
@@ -1,7 +1,8 @@
 import requests
 
-#r = requests.get('https://api.github.com/events')
-r = requests.post('https://httpbin.org/post', data = {'key':'value'})  
+from bitrix24 import Bitrix24
 
+bx24 = Bitrix24('YOUR_THIRD_LEVEL_DOMAIN', 'YOUR_AUTH_TOKEN')
 
-print(r.text)
+print(bx24.call('app.info'))
+
